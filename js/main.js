eb2d4ce25d456a819698292e326bcecfd4f3aa2d
Nehal128 <nehalmohamed821@gmail.com>
2022-01-04 01:05:22 +0200

    uploads project files

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// $(document).ready(function () {
+//   AOS.init();
+//   var hswiper = new Swiper("header .mySwiper", {
+//     pagination: {
+//       el: ".swiper-pagination",
+//       type: "fraction",
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+//   $(function () {
+//     $('[data-toggle="tooltip"]').tooltip();
+//   });
+//   hswiper.on('activeIndexChange', function () {
+//     AOS.init({ 
+//         animatedClassName: 'animate'
+//     });
+//   });
+  
+// });
+
